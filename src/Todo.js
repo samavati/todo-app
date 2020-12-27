4e5c9baa8c02ff4855ea1bdfc66b3f000e3d279b
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-27 04:48:48 +0000

    todo item added

---
+++ src/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Todo extends React.Component {
+
+    render() {
+        return (
+            <li className="list-group-item">
+                <span>{this.props.date}</span>
+                <input className="form-check-input mx-2" type="checkbox" value="" aria-label="..." />
+                {this.props.title}
+                <button type="button" className="btn bg-transparent">
+                    <i className="far fa-edit"></i>
+                </button>
+            </li>
+        );
+    }
+}
+
+export default Todo;
