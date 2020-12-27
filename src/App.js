9d2924b593b734dd528229b811ad4c647a3e20bc
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-27 05:01:52 +0000

    todo item info added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import TodoItemInfo from './TodoItemInfo';
 import TodoList from './TodoList';
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
       <div className="row">
         <div className="col-12 col-md-6">
           <TodoList />
+        </div>
+        <div className="col-12 col-md-6">
+          <TodoItemInfo />
         </div>
       </div>
     </div>
