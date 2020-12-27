f2bac233bfff852f7159c6fbf280818b8768327e
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-27 05:13:54 +0000

    top navbar added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import TodoList from './TodoList';
 
 function App() {
   return (
-    <div className="container-fluid container-md">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <TodoList />
-        </div>
-        <div className="col-12 col-md-6">
-          <TodoItemInfo />
+    <>
+      <Navbar />
+      <div className="container-fluid container-md">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <TodoList />
+          </div>
+          <div className="col-12 col-md-6">
+            <TodoItemInfo />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand">Navbar</a>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </div>
+        </nav>
+    );
+}
