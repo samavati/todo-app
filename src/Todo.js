ac116df7003ca36533ebce6f25cc58170cb0bffc
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-27 04:54:20 +0000

    edit button style

--- src/Todo.js
+++ src/Todo.js
@@ -4,11 +4,11 @@ class Todo extends React.Component {
 
     render() {
         return (
-            <li className="list-group-item">
+            <li className="list-group-item d-flex align-items-center">
                 <span>{this.props.date}</span>
                 <input className="form-check-input mx-2" type="checkbox" value="" aria-label="..." />
                 {this.props.title}
-                <button type="button" className="btn bg-transparent">
+                <button type="button" className="btn bg-transparent ms-auto">
                     <i className="far fa-edit"></i>
                 </button>
             </li>
