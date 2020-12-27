2d65dc92f3f5a81221c6fccc2e7c3d55a7965d4d
Ehsan Samavati <samavati.ehsan@yahoo.com>
2020-12-27 05:11:27 +0000

    use standard form

--- src/TodoItemInfo.js
+++ src/TodoItemInfo.js
@@ -5,17 +5,16 @@ class TodoItemInfo extends React.Component {
         return (
             <form>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <label htmlFor="todoTitle" className="form-label">Todo Title</label>
+                    <input type="text" className="form-control" id="todoTitle" />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <label htmlFor="todoDescription" className="form-label">Todo Description</label>
+                    <textarea className="form-control" id="todoDescription" rows="3"></textarea>
                 </div>
                 <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    <input type="checkbox" className="form-check-input" id="done" />
+                    <label className="form-check-label" htmlFor="done">done</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
